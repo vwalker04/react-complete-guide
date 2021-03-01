fa6e97ec09c35b9db54eb6d0d7cf4f7df32a0e0f
Vaughn Walker <vwalker04@yahoo.com>
2021-03-01 09:22:12 -0500

    Up to date with lesson 38

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Vaughn', age: 34},
+            {name: 'Andrea', age: 34},
+            {name: 'Ronin', age: 5}
+        ]
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <Person name={"Vaughn"} age={Math.floor(Math.random() * 100)}><p>hey</p></Person>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This works!</p>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+    }
 }
 
 export default App;
